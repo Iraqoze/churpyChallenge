aae0770db344bb9d6431295ba00e153e66f9966c
Irakoze Lionel <iraqoze.lionel@gmail.com>
2023-01-11 00:43:11 +0300

    optimized concatenator function from N^4 to N^2

--- concatenator.js
+++ concatenator.js
@@ -1,26 +1,39 @@
-function generate(input_array) {
-    
-    let result = []; // Initialize an empty array to store the final output
-    
-    // outer loop to iterate over the first array
-    for (let i = 0; i < input_array.length; i++) {
-        // inner loop to iterate over elements of the first array
-        for (let j = 0; j < input_array[i].length; j++) {
-            // Third loop to iterate over second array
-            for (let k = i + 1; k < input_array.length; k++) {
-                // Fourth loop to iterate over elements of the second array
-                for (let l = 0; l < input_array[k].length; l++) {
+function generate(input_array)
+{
+    let result = [];  // Initialize an empty array to store the final output
+
+    // outer forEach loop to iterate over the first array
+    input_array.forEach((arr1, i) =>
+    {
+
+        // inner forEach loop to iterate over elements of the first array
+        arr1.forEach(x =>
+        {
+
+            // Using the Array.prototype.slice() method to create a new array
+            // containing all elements from the input_array after the element at index i
+            let slicedArray = input_array.slice(i + 1);
+
+            // Third forEach loop to iterate over second array
+            slicedArray.forEach(arr2 =>
+            {
+
+                // Fourth forEach loop to iterate over elements of the second array
+                arr2.forEach(y =>
+                {
                     // Concatenate elements from first and second array and push the result into result array
-                    result.push(`${input_array[i][j]} ${input_array[k][l]}`);
-                }
-            }
-        }
-    }
+                    result.push(`${x} ${y}`);
+                });
+            });
+        });
+    });
     // Return the final output array
     return result;
 }
 
 
+
 // Testing
-let arr = [[1, 2, 3], [4, 5, 6]];
+//let arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+let arr = [[1, 2, 3], [4, 5, 6]]
 console.log(generate(arr));
