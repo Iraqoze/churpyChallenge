1374dd9645ba5686851067b61cf22071283f136b
Irakoze Lionel <iraqoze.lionel@gmail.com>
2023-01-11 10:34:31 +0300

    final

--- parser.js
+++ parser.js
@@ -1,6 +1,7 @@
 //Function to evaluate ternary expressions
-function ternaryOperator(input, variables) {
-	
+function ternaryOperator(input, variables)
+{
+
 	console.log('INPUT OR EXPRESSION: ', input)
 
 	// Define the regex pattern to match ternary operations
@@ -8,13 +9,14 @@ function ternaryOperator(input, variables) {
 
 	const match = input.match(pattern)
 
-	if (match) {
+	if (match)
+	{
 		// Extract the condition, truthy value, and falsy value from the match
 		const condition = match[1]
 		const truthy = match[2]
 		const falsy = match[3]
 
-		//Looging the values of extracted:
+		//Logging the values extracted:
 		console.log('\nCondition', condition)
 		console.log('\nTruthy', truthy)
 		console.log('\nFalsy', falsy)
@@ -22,28 +24,33 @@ function ternaryOperator(input, variables) {
 		// iterates over the keys of the variables object  
 		//for each key in the loop, replace the key with its corresponding value in the toEval string.
 		let toEval = condition
-		Object.keys(variables).forEach((key) => {
+		Object.keys(variables).forEach((key) =>
+		{
 			toEval = toEval.replace(new RegExp(key, 'g'), variables[key])
 		})
 
 		// Evaluate the condition and return the truthy/falsy value		
-		if (eval(toEval)) {
+		if (eval(toEval))
+		{
 
-			console.log("EVALUATING CONDITION: ",toEval);
-			console.log("TRUTHY VALUE: ",truthy);
+			console.log("EVALUATING CONDITION: ", toEval);
+			console.log("TRUTHY VALUE: ", truthy);
 
 			//call the ternary operator function recursively using truthy value and variables
 			return ternaryOperator(truthy, variables)
-		} else {
+		} else
+		{
 
-			console.log("EVALUATING CONDITION: ",toEval);
-			console.log("FALSY VALUE: ",falsy);
+			console.log("EVALUATING CONDITION: ", toEval);
+			console.log("FALSY VALUE: ", falsy);
 
 			//call the ternary operator function recursively using falsy value and variables
 			return ternaryOperator(falsy, variables)
 		}
-		} else {
-		if (!isNaN(Number(input))) {
+	} else
+	{
+		if (!isNaN(Number(input)))
+		{
 			return Number(input)
 		}
 		// If the input does not match the regular expression, it must be a variable name
@@ -52,31 +59,42 @@ function ternaryOperator(input, variables) {
 	}
 }
 
-function run(input, variables) {
+function run(input, variables)
+{
 
-	if (!input.startsWith('+') && !input.startsWith('-')) {
+	if (!input.startsWith('+') && !input.startsWith('-'))
+	{
 		input = '+' + input
 	}
+
 	let result = 0
+
 	const pattern = /([+-])\s*(.*)/
 
-	while (input.length > 0) {
+	while (input.length > 0)
+	{
 		const match = input.match(pattern)
-		if (!match) break
+
+		if (!match)
+			break
+
 		input = input.substring(match[0].length)
 		const operator = match[1]
 		const operand = match[2]
-		// console.log('Operator: ', operator)
 
-		if (operator === '+') {
+
+		if (operator === '+')
+		{
 			// If the operator is "+", add the operand to the result
 			result += Number(ternaryOperator(operand, variables))
-		} else if (operator === '-') {
+		} else if (operator === '-')
+		{
 			// If the operator is "-", subtract the operand from the result
 			result -= Number(ternaryOperator(operand, variables))
 		}
 	}
 	// Return the final result
+	console.log("\n\n------------Answer-------------\n")
 	return result
 }
 
@@ -90,6 +108,7 @@ const values = {
 	var_3: 3,
 	var_4: 5,
 }
+
 
 console.log(run(input, values)) // Output: 15
 
